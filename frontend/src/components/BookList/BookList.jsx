ee4bf0d3ddacd83d6dc4676ab0de547235a8eb80
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-11-25 18:55:49 +0300

    Добавить генерацию id книг

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -5,15 +5,16 @@ function BookList() {
     const books = useSelector((state) => state.books);
     return (
         <section className="book-list block">
-            <h2 className='title-2'>Book List</h2>
+            <h2 className="title-2">Book List</h2>
             {books.length === 0 ? (
                 <p>No books available</p>
             ) : (
                 <ul>
                     {books.map((book, index) => (
-                        <li key={index}>
+                        <li key={book.id}>
                             <div className="book-list__info">
-                                {++index}. {book.title} by <strong>{book.author}</strong>
+                                {++index}. {book.title} by{' '}
+                                <strong>{book.author}</strong>
                             </div>
                         </li>
                     ))}
