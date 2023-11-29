70cbcded908a8f966063b3c6b4b81f04fc9a275f
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-11-29 16:12:45 +0300

    Добавить extraReducers в booksSlice

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addBook, thunkFunction } from '../../redux/slices/booksSlice';
+import { addBook, fetchBook } from '../../redux/slices/booksSlice';
 import createBookWithID from '../../utils/createBookWithID';
 import booksData from '../../data/books.json';
 import './BookForm.scss';
@@ -27,7 +27,7 @@ function BookForm() {
     };
 
     const handleAddRandomBookViaAPI = () => {
-        dispatch(thunkFunction);
+        dispatch(fetchBook());
     };
 
     return (
